73fb09a39e582e09dbe7db9db3f1c40042ea4a64
Chris Song <chris.bk.song@gmail.com>
2020-07-01 15:54:06 -0400

    saving progress to repository

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,7 +4,7 @@ import RecipeDetail from './RecipeDetail';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Container, Button, Nav } from 'react-bootstrap';
+import { Container, Button, Form, Navbar } from 'react-bootstrap';
 
 import styles from './Recipe.module.css';
 
@@ -29,7 +29,7 @@ export default class Recipe extends Component {
   handleFormSubmit = (e) => {
     e.preventDefault();
     let recipe = this.state.recipeName
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=644cf81819bd45b8a428df385604dc1c&query=${recipe}`)
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=67c41709350d4b19bd7705edd26e778a&query=${recipe}`)
     // fetch(`https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=644cf81819bd45b8a428df385604dc1c&includeNutrition=true`)
     .then(res => res.json())
     .then(data => {
@@ -45,18 +45,17 @@ export default class Recipe extends Component {
   render() {
     return (
       <div className={styles.Background}>
-        <Nav className={styles.NavBar}>
-          <h1>Salt and Pepper</h1>
-          <form onSubmit={ this.handleFormSubmit } >
-            <input type="text" id="recipe" value={ this.state.recipeName } onChange={ this.handleChange} placeholder="Search for recipes">
-            </input>
-            <Button type="submit">Search</Button>
-          </form>
-          <a href="/login"><Button>Login</Button></a>
-          <a href="/signup"><Button>SignUp</Button></a>
-        </Nav>
-        <header>
-        </header>
+        <Navbar bg="light" expand="lg" className={styles.NavBarNew}>
+        <Navbar.Brand href="#home"><b>Salt & Pepper</b></Navbar.Brand>
+          <Form inline onSubmit={ this.handleFormSubmit } autocomplete="off">
+            <input className="mr-sm-2" type="text" id="recipe" value={ this.state.recipeName } onChange={ this.handleChange} placeholder="Search for recipes"></input>
+            <Button variant="outline-success" className="btn-sm" type="submit">Search</Button>
+          </Form>
+            <a href="/login"><Button className="btn-sm">Login</Button></a>
+        </Navbar>
+        <Container className={styles.Image}>
+          <img src="https://www.pexels.com/photo/board-bunch-cooking-food-349609/" alt="cooking board" />
+        </Container>
         <Container className={styles.RecipeBox}>
           { this.state.recipes.map((recipe, index) => {
             return (
--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Card } from 'react-bootstrap'
+import { Container, Card, Button } from 'react-bootstrap'
 import styles from './RecipeDetail.module.css'
 
 class RecipeDetail extends Component {
   state = {data: null}
   componentDidMount(){
-    fetch(`https://api.spoonacular.com/recipes/${this.props.recipe.id}/information?apiKey=644cf81819bd45b8a428df385604dc1c&includeNutrition=true`)
+    fetch(`https://api.spoonacular.com/recipes/${this.props.recipe.id}/information?apiKey=67c41709350d4b19bd7705edd26e778a&includeNutrition=true`)
     .then(res => res.json())
     .then(data => {
       this.setState({
@@ -18,16 +18,20 @@ class RecipeDetail extends Component {
 
   render(){
     return (
-      <Card className={styles.Card}>
-        <img src={this.props.recipe.image} alt=""/>
-        <div>
-          <h5>{this.props.recipe.title}</h5>
-          <h5>{this.props.recipe.id}</h5>
-          { this.state.data && (
-            <div>{ this.state.data.creditsText }</div> 
-          )}
-        </div>
-      </Card>
+      <Container className={styles.Container}>
+        <Card className={styles.Card}>
+          <img src={this.props.recipe.image} alt=""/>
+          <div>
+            <h5>{this.props.recipe.title}</h5>
+            {/* <h5>{this.props.recipe.id}</h5> */}
+            { this.state.data && (
+              <div>{ this.state.data.instructions }</div> 
+            )}
+          </div>
+          <a href="/Detail"><Button>Detail</Button></a>
+          <Button>Save!</Button>
+        </Card>
+      </Container>
     )  
   }
 }
